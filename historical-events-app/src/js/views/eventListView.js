5a9c162b1834b93fe43b5fb33b58c2d3b61154cd
André <andre@Andres-MacBook-Pro.local>
2026-01-30 09:20:18 -0300

    new view

--- historical-events-app/src/js/views/eventListView.js
+++ historical-events-app/src/js/views/eventListView.js
@@ -7,6 +7,13 @@ class EventListView extends View {
     document.querySelector(".section__search-input").value
   ).getFullYear();
 
+  _generateMarkup() {
+    return `
+    
+    
+    `;
+  }
+
   addSearchHandler(handler, searchValue) {
     this._searchButton.addEventListener("click", function (e) {
       e.preventDefault();
